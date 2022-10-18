108e6b43d22eec7d1bf854acb824831182cbfd15
matyasbejo <bejomatyi@gmail.com>
2022-10-18 19:45:45 +0200

    IRepository interface added

---
+++ ENOMVG_HFT_2022231.Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using ENOMVG_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENOMVG_HFT_2022231.Repository
+{
+    public interface IRepository
+    {
+        void Create(School school);
+        School Read(int id);
+        IQueryable<School> ReadAll();
+        void Update(School school);
+        void Delete(int id);
+    }
+}
