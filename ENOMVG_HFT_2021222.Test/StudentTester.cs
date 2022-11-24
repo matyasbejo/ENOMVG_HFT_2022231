06e34d569a4b5ef71d322588c596a2ccaeecf65a
matyasbejo <bejomatyi@gmail.com>
2022-11-24 22:21:31 +0100

    Added new Tester file "StudentTester"

---
+++ ENOMVG_HFT_2021222.Test/StudentTester.cs
@@ -0,0 +1,37 @@
+﻿using ENOMVG_HFT_2022231.Logic;
+using ENOMVG_HFT_2022231.Models;
+using ENOMVG_HFT_2022231.Repository;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENOMVG_HFT_2022231.Test
+{
+    [TestFixture]
+    internal class StudentTester
+    {
+        SchoolLogic logic;
+        Mock<IRepository<Student>> MockStudentRepository;
+
+        [SetUp]
+        public void Init()
+        {
+            var input = new List<Student>()
+            {
+                new Student(1, "Elso Diak", 17, 1, 5),
+                new Student(2, "Masodik Diak", 7, 2, 3),
+                new Student(3, "Harmadik Diak", 14, 1, 3.5)
+            }.AsQueryable();
+            MockStudentRepository = new Mock<IRepository<Student>>();
+            MockStudentRepository.Setup(s => s.ReadAll()).Returns(input);
+            MockStudentRepository.Setup(s => s.Read(1)).Returns(input.First(st => st.Id == 1));
+            MockStudentRepository.Setup(s => s.Read(2)).Returns(input.First(s => s.Id == 2));
+            MockStudentRepository.Setup(s => s.Read(3)).Returns(input.First(s => s.Id == 3));
+            logic = new SchoolLogic(MockStudentRepository.Object);
+        }
+    }
+}
