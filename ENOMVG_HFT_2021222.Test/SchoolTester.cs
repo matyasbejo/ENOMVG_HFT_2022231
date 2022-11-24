eb1c51ace42e7ef42a75523b9541da37ccf5ad85
matyasbejo <bejomatyi@gmail.com>
2022-11-24 22:02:45 +0100

    Renamed Tester.cs to SchoolTester.cs

---
+++ ENOMVG_HFT_2021222.Test/SchoolTester.cs
@@ -0,0 +1,84 @@
+﻿using ENOMVG_HFT_2022231.Logic;
+using ENOMVG_HFT_2022231.Models;
+using ENOMVG_HFT_2022231.Repository;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENOMVG_HFT_2022231.Test
+{
+    [TestFixture]
+    internal class Tester
+    {
+        SchoolLogic logic;
+        Mock<IRepository<School>> MockSchoolRepository;
+
+        [SetUp]
+        public void Init()
+        {
+            var input = new List<School>()
+            {
+                new School(1, "Elso Varosi Iskola", stype.Primary),
+                new School(2, "Masodik Varosi Iskola", stype.High),
+                new School(3, "Harmadik Varosi Iskola", stype.High)
+            }.AsQueryable();
+            MockSchoolRepository = new Mock<IRepository<School>>();
+            MockSchoolRepository.Setup(s => s.ReadAll()).Returns(input);
+            MockSchoolRepository.Setup(s => s.Read(1)).Returns(input.First(s => s.Id == 1));
+            MockSchoolRepository.Setup(s => s.Read(2)).Returns(input.First(s => s.Id == 2));
+            MockSchoolRepository.Setup(s => s.Read(3)).Returns(input.First(s => s.Id == 3));
+            logic = new SchoolLogic(MockSchoolRepository.Object);
+        }
+
+        [Test]
+        public void ReadNameTestWithExistingSchool()
+        {
+            School sch = logic.ReadName("Masodik Varosi Iskola");
+
+            Assert.That(sch.Equals(logic.Read(2)));
+        }
+
+        [Test]
+        public void ReadNameTestWithFakeSchool()
+        {
+            Assert.Throws<InvalidOperationException>(() => { logic.ReadName("exterminate"); });
+        }
+
+        [Test]
+        public void CreateNewSchoolTest()
+        {
+
+            var school = new School("Otodik Varosi Iskola", stype.Secondary);
+
+            logic.Create(school);
+
+            MockSchoolRepository.Verify(r => r.Create(school), Times.Once());
+        }
+
+        [Test]
+        public void ReadExistingSchoolTest()
+        {
+            logic = new SchoolLogic(MockSchoolRepository.Object);
+            var school = logic.Read(1);
+            MockSchoolRepository.Verify(r => r.Read(1), Times.Once());
+        }
+
+        [Test]
+        public void ReadNotExistingTest()
+        {
+            try
+            {
+                var school = logic.Read(33);
+            }
+            catch (Exception ex) { }
+
+            MockSchoolRepository.Verify(r => r.Read(3), Times.Never());
+            Assert.Throws<Exception>(() => logic.Read(33));
+        }
+
+    }
+}
