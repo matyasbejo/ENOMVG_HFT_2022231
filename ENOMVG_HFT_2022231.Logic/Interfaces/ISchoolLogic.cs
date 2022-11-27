6c3df56c7b46be044d71de50ad673082b9454c4e
matyasbejo <bejomatyi@gmail.com>
2022-11-27 21:14:10 +0100

    Interface refreshed

--- ENOMVG_HFT_2022231.Logic/Interfaces/ISchoolLogic.cs
+++ ENOMVG_HFT_2022231.Logic/Interfaces/ISchoolLogic.cs
@@ -8,10 +8,13 @@ namespace ENOMVG_HFT_2022231.Logic
         int CountAll(int _id);
         void Create(School _item);
         void Delete(int _id);
-        double GetSchoolAVG(int _schoolId);
         School Read(int _id);
         IQueryable<School> ReadAll();
         School ReadName(string _name);
+        double SchoolGradesAVG(int _schoolId);
+        int SchoolSalaryAVG(int _SchoolId);
+        IEnumerable<Student> StudentsOfSchool(int _schoolId);
+        IEnumerable<Teacher> TeachersOfSchool(int _schoolId);
         void Update(School _school);
     }
 }
