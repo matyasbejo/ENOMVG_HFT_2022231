c7beca7a1893be09077bab1a8bb83aa06e4ec327
matyasbejo <bejomatyi@gmail.com>
2022-11-27 23:31:34 +0100

    Added exceptionhandler to the endpoint application

--- ENOMVG_HFT_2022231.Endpoint/Startup.cs
+++ ENOMVG_HFT_2022231.Endpoint/Startup.cs
@@ -2,7 +2,9 @@ using ENOMVG_HFT_2022231.Logic;
 using ENOMVG_HFT_2022231.Models;
 using ENOMVG_HFT_2022231.Repository;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -55,6 +57,15 @@ namespace ENOMVG_HFT_2022231.Endpoint
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ENOMVG_HFT_2022231.Endpoint v1"));
             }
+
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                var exception = context.Features
+                .Get<IExceptionHandlerPathFeature>()
+                .Error;
+                var response = new { error = exception.Message };
+                await context.Response.WriteAsJsonAsync(response);
+            }));
 
             app.UseRouting();
 
