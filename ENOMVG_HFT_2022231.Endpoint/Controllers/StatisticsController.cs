ea25118aef23dc06bb5dc250fdc8434dca471830
matyasbejo <bejomatyi@gmail.com>
2022-11-27 21:06:01 +0100

    Added endpoint controller for non crud methods

---
+++ ENOMVG_HFT_2022231.Endpoint/Controllers/StatisticsController.cs
@@ -0,0 +1,17 @@
+﻿using ENOMVG_HFT_2022231.Logic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ENOMVG_HFT_2022231.Endpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class StatisticsController : ControllerBase
+    {
+        ISchoolLogic SchoolLogic;
+
+        public StatisticsController(ISchoolLogic _schoollogic)
+        {
+            this.SchoolLogic = _schoollogic;
+        }
+    }
+}
