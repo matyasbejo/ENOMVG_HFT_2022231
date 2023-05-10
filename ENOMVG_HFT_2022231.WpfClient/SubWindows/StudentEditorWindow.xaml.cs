38a8ebb5e442e820a8847ec6f7f4d26228aa25cf
matyasbejo <bejomatyi@gmail.com>
2023-05-10 16:17:19 +0200

    StudentEditorWindow + VM added

---
+++ ENOMVG_HFT_2022231.WpfClient/SubWindows/StudentEditorWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ENOMVG_HFT_2022231.WpfClient.SubWindows
+{
+    /// <summary>
+    /// Interaction logic for StudentEditorWindow.xaml
+    /// </summary>
+    public partial class StudentEditorWindow : Window
+    {
+        public StudentEditorWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
