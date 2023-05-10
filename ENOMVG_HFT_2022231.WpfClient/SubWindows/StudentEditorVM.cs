643583d3adf94c95fc7e6f5957290097a945c044
matyasbejo <bejomatyi@gmail.com>
2023-05-10 16:33:12 +0200

    First editr window created

--- ENOMVG_HFT_2022231.WpfClient/SubWindows/StudentEditorVM.cs
+++ ENOMVG_HFT_2022231.WpfClient/SubWindows/StudentEditorVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ENOMVG_HFT_2022231.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace ENOMVG_HFT_2022231.WpfClient.SubWindows
 {
     class StudentEditorVM
     {
+        public RestCollection<Student> Students { get; set; }
+        public StudentEditorVM()
+        {
+            Students = new RestCollection<Student>("http://localhost:15398/", "student");
+        }
     }
 }
