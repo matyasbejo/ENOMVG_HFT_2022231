76eeb1467d9994eaf403a91865d74fe34d9b752c
matyasbejo <bejomatyi@gmail.com>
2023-05-10 17:34:02 +0200

    Create, delete, update commands initcreate testdesign mode check

--- ENOMVG_HFT_2022231.WpfClient/SubWindows/StudentEditorVM.cs
+++ ENOMVG_HFT_2022231.WpfClient/SubWindows/StudentEditorVM.cs
@@ -1,18 +1,46 @@
 ﻿using ENOMVG_HFT_2022231.Models;
+using Microsoft.Toolkit.Mvvm.DependencyInjection;
+using Microsoft.Toolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
 
 namespace ENOMVG_HFT_2022231.WpfClient.SubWindows
 {
     class StudentEditorVM
     {
         public RestCollection<Student> Students { get; set; }
+
+        public ICommand CreateStudentCommand { get; set; }
+        public ICommand DeleteStudentCommand { get; set; }
+        public ICommand UpdateStudentCommand { get; set; }
+
         public StudentEditorVM()
         {
-            Students = new RestCollection<Student>("http://localhost:15398/", "student");
+            if (!IsInDesignMode)
+            {
+                Students = new RestCollection<Student>("http://localhost:15398/", "student");
+                CreateStudentCommand = new RelayCommand(() =>
+                    {
+                        Student s = new Student();
+                        s.Name = "sfl";
+                        Students.Add(s);
+                    });
+            }
+        }
+        public static bool IsInDesignMode
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
+            }
         }
     }
 }
