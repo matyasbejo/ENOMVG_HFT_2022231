ea5fbf54c96fdeff7213f19452b0e119b08e2318
matyasbejo <bejomatyi@gmail.com>
2023-05-29 21:48:11 +0200

    signalR added to js

--- ENOMVG_HFT_2022231.JSClient2/wwwroot/script.js
+++ ENOMVG_HFT_2022231.JSClient2/wwwroot/script.js
@@ -1,6 +1,37 @@
 ﻿let students = [];
-
+let connection = null;
 getdata();
+setupSignalR();
+
+function setupSignalR() {
+    connection = new signalR.HubConnectionBuilder()
+        .withUrl("http://localhost:15398/hub")
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+
+    connection.on("StudentCreated", (user, message) => {
+        getdata();
+    });
+
+    connection.on("StudentDeleted", (user, message) => {
+        getdata();
+    });
+
+    connection.onclose(async () => {
+        await start();
+    });
+    start();
+}
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("SignalR Connected.");
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+};
 
 async function getdata() {
     await fetch('http://localhost:15398/student')
