cb286a125cff229d1c34d6bea302a900b2cf7e98
matyasbejo <bejomatyi@gmail.com>
2023-05-29 20:02:31 +0200

    school page, list function implemented

--- ENOMVG_HFT_2022231.JSClient2/wwwroot/script.js
+++ ENOMVG_HFT_2022231.JSClient2/wwwroot/script.js
@@ -1,1 +1,19 @@
-﻿
+﻿let students = [];
+
+fetch('http://localhost:15398/student')
+    .then(x => x.json())
+    .then(y => {
+        students = y;
+        console.log(y);
+        display();
+    });
+
+function display() {
+    students.forEach(s => {
+        document.getElementById("StudentGetArea").innerHTML +=
+            "<tr><th>" + s.name +
+            "</th><th>" + s.schoolId +
+            "</th><th>" + s.age +
+            "</th><th>" + s.gradesAVG + "</th></tr>";
+    });
+}
